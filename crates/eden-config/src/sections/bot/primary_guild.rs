5e58f961894362501c69fcac1fb567fc04932b11
memothelemo <dev@memothelemo.xyz>
2026-03-20 22:47:03 +0800

    Implement admin command alert response

--- crates/eden-config/src/sections/bot/primary_guild.rs
+++ crates/eden-config/src/sections/bot/primary_guild.rs
@@ -1,9 +1,10 @@
 use serde::Deserialize;
 use twilight_model::id::Id;
-use twilight_model::id::marker::GuildMarker;
+use twilight_model::id::marker::{ChannelMarker, GuildMarker};
 
 /// Configuration for the primary guild for Eden.
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub struct PrimaryGuild {
     pub id: Id<GuildMarker>,
+    pub alert_channel_id: Option<Id<ChannelMarker>>,
 }
--- src/core/src/jobs/alerts/admin_command.rs
+++ src/core/src/jobs/alerts/admin_command.rs
@@ -1,12 +1,16 @@
 use eden_background_worker::BackgroundJob;
+use eden_database::Timestamp;
 use eden_gateway_api::alerts::admin_commands::{
-    AdminCommandAlert as EncodedAdminCommandAlert, Executor,
+    AdminCommandAlert as EncodedAdminCommandAlert, Executor, ExecutorPlayerInfo,
 };
+use eden_text_handling::markdown::strip_markdown;
 use eden_twilight::http::ResponseFutureExt;
 use erased_report::ErasedReport;
 use serde::{Deserialize, Serialize};
 use std::{borrow::Cow, sync::Arc, time::Duration};
-use twilight_model::id::Id;
+use twilight_util::builder::embed::{
+    EmbedAuthorBuilder, EmbedBuilder, EmbedFieldBuilder, ImageSource,
+};
 
 use crate::jobs::JobContext;
 
@@ -21,31 +25,102 @@ impl BackgroundJob for AdminCommandAlertJob {
     type Context = Arc<JobContext>;
 
     #[tracing::instrument(skip_all, fields(
-        alert.command = ?self.command,
-        alert.executor = ?self.executor,
+        alert.command = ?self.0.command,
+        alert.executor = ?self.0.executor,
     ))]
     async fn run(&self, ctx: Self::Context) -> Result<(), ErasedReport> {
-        // let content = match &self.executor {
-        //     Executor::Console => Cow::Borrowed("**Someone used a privileged command!!**"),
-        //     Executor::Player(info) => {
-        //         Cow::Owned(format!("**{} used a privileged command!!**", info.username))
-        //     }
-        // };
+        let alert = &self.0;
+        let Some(alert_channel_id) = ctx.kernel.config.bot.primary_guild.alert_channel_id else {
+            return Ok(());
+        };
 
-        // let request = ctx
-        //     .discord
-        //     .create_message(todo!())
-        //     .content(&content);
+        let partial_embed = EmbedBuilder::new()
+            .title(format!("`{}`", strip_markdown(&alert.command)))
+            .timestamp(Timestamp::now().into_twilight());
 
-        // request.perform().await?;
+        let (content, embed) = match &alert.executor {
+            Executor::Console => {
+                let content = Cow::Borrowed("**Someone used a privileged command!!**");
+                let embed = partial_embed.author(EmbedAuthorBuilder::new("Console").build());
+                (content, embed)
+            }
+            Executor::Player(info) => {
+                let content = format!("**{} used a privileged command!!**", info.username);
+                let embed = embed_for_player_executor(partial_embed, info);
+                (Cow::Owned(content), embed)
+            }
+        };
+
+        let embed = embed.build();
+        ctx.discord
+            .create_message(alert_channel_id)
+            .content(&content)
+            .embeds(&[embed])
+            .perform()
+            .await?;
+
         Ok(())
     }
 }
 
-impl std::ops::Deref for AdminCommandAlertJob {
-    type Target = EncodedAdminCommandAlert;
+fn embed_for_player_executor(embed: EmbedBuilder, player: &ExecutorPlayerInfo) -> EmbedBuilder {
+    let icon_url = ImageSource::url(get_head_icon_url(&player.username))
+        .expect("get_head_icon_url should produce valid URL");
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+    let author_field = EmbedAuthorBuilder::new(&player.username)
+        .icon_url(icon_url)
+        .build();
+
+    let uuid_field = EmbedFieldBuilder::new("UUID", format!("`{}`", player.uuid)).inline();
+    let dim_field = EmbedFieldBuilder::new("Dimension", format!("`{}`", player.dimension)).inline();
+    let position_field = EmbedFieldBuilder::new(
+        "Position",
+        format!(
+            "`{}, {}, {}`",
+            player.position.x, player.position.y, player.position.z
+        ),
+    )
+    .inline();
+
+    let gamemode_field =
+        EmbedFieldBuilder::new("Game Mode", format!("`{}`", player.gamemode)).inline();
+
+    embed
+        .author(author_field)
+        .field(uuid_field)
+        .field(dim_field)
+        .field(position_field)
+        .field(gamemode_field)
+}
+
+fn get_head_icon_url(username: &str) -> String {
+    const HEAD_ICON_BASE_URL: &str = "https://minotar.net/avatar/";
+
+    let mut url = HEAD_ICON_BASE_URL.to_string();
+    url.extend(percent_encoding::percent_encode(
+        username.as_bytes(),
+        percent_encoding::NON_ALPHANUMERIC,
+    ));
+    url
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_head_icon_url;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_get_head_icon_url() {
+        assert_eq!(
+            get_head_icon_url("Notch"),
+            "https://minotar.net/avatar/Notch"
+        );
+
+        // Bedrock supports usernames with spaces, so the username
+        // must be percent-encoded to produce a valid URL.
+        assert_eq!(
+            get_head_icon_url("Ordinary Player"),
+            "https://minotar.net/avatar/Ordinary%20Player"
+        );
     }
 }
